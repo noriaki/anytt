58492cafac1a846b26af004f8cd3a88155f72733
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-12-24 20:46:42 +0900

    add ignore patterns of jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
+const pathIgnorePatterns = ['<rootDir>/.git/', '<rootDir>/.next/'];
+const nodeModulesPattern = '<rootDir>/node_modules/';
+
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
+  // setupFilesAfterEnv: [
+  //   './jest.setup.js',
+  // ],
+  testPathIgnorePatterns: [...pathIgnorePatterns, nodeModulesPattern],
+  transformIgnorePatterns: [...pathIgnorePatterns, nodeModulesPattern],
+  watchPathIgnorePatterns: [...pathIgnorePatterns, nodeModulesPattern],
+  modulePathIgnorePatterns: pathIgnorePatterns,
 };
