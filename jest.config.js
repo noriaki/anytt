019e80a9f891777ae2029f8e0efc826bbc7ff175
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-12-27 14:51:40 +0900

    add enzyme setup

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,7 @@ const nodeModulesPattern = '<rootDir>/node_modules/';
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
-  // setupFilesAfterEnv: [
-  //   './jest.setup.js',
-  // ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
   testPathIgnorePatterns: [...pathIgnorePatterns, nodeModulesPattern],
   transformIgnorePatterns: [...pathIgnorePatterns, nodeModulesPattern],
   watchPathIgnorePatterns: [...pathIgnorePatterns, nodeModulesPattern],
