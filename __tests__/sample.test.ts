ccf598658c79f986b05255e344a864bda7412949
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-19 21:15:03 +0900

    add sample passed test to enable renovate automerge

---
+++ __tests__/sample.test.ts
@@ -0,0 +1,17 @@
+import { compilerOptions } from '../tsconfig-jest.json';
+
+const sum = (a: number, b: number): number => a + b;
+
+describe('sample', () => {
+  it('1 + 2 = 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('sum(1, 2) = 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  it('tsconfig.compilerOptions.jsx === react', () => {
+    expect(compilerOptions.jsx).toBe('react');
+  });
+});
