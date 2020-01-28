a19808874f29c07047894e7a449d55892cc40ace
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-28 17:00:19 +0900

    add initial material-ui components by Next.js

---
+++ src/contexts/theme.ts
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: blue.A400,
+    },
+    background: {
+      default: '#e9e9e9',
+    },
+  },
+});
+
+export default theme;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => (
+  <div>
+    <h1>Hello Next.js with TypeScript</h1>
+  </div>
+);
+
+export default IndexPage;
