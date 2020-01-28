5b74ce57a821ef4f735787989db7b57d47a9ad01
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-28 13:14:33 +0900

    refactor: add fixtures and separates logic to helper

--- __tests__/data/models/associations.test.ts
+++ __tests__/data/models/associations.test.ts
@@ -1,36 +1,37 @@
-import { Connection, Model, Document } from 'mongoose';
-
-import connectMongoDB from '~/data/db';
+import connectDB, { disconnect as disconnectDB, MongooseModel } from '~/data/db';
+import { deleteModels, dropCollections, importFixtures } from '~/data/fixtures/helper';
 import { ResidenceDocument } from '~/data/models/Residence';
 
-type MongooseModel = Model<Document, {}>;
-
 describe('associations between Residence and Stop', () => {
-  let connection: Connection;
   let Residence: MongooseModel;
   let Stop: MongooseModel;
   let Routing: MongooseModel;
 
-  beforeEach(async () => {
-    const ret = await connectMongoDB();
-    connection = ret.connection;
-    Residence = ret.models.Residence;
-    Stop = ret.models.Stop;
-    Routing = ret.models.Routing;
-    await Promise.all([
-      Residence.deleteMany({}),
-      Stop.deleteMany({}),
-      Routing.deleteMany({}),
-    ]);
-    await makeFixtures(Residence, Stop, Routing);
+  beforeAll(async () => {
+    await connectDB();
   });
 
-  afterEach(() => connection.close());
+  beforeEach(async () => {
+    const { models } = await connectDB();
+    await importFixtures();
+    Residence = models.Residence;
+    Stop = models.Stop;
+    Routing = models.Routing;
+  });
+
+  afterEach(async () => {
+    await deleteModels();
+    await dropCollections();
+  });
+
+  afterAll(async () => {
+    await disconnectDB();
+  });
 
   it('count', async () => {
     expect(await Residence.countDocuments()).toBe(2);
-    expect(await Stop.countDocuments()).toBe(2);
-    expect(await Routing.countDocuments()).toBe(3);
+    expect(await Stop.countDocuments()).toBe(3);
+    expect(await Routing.countDocuments()).toBe(4);
   });
 
   it('populate', async () => {
@@ -40,35 +41,6 @@ describe('associations between Residence and Stop', () => {
     })) as ResidenceDocument;
     expect(dt).not.toBeNull();
     expect(dt).toHaveProperty('stops');
-    expect(dt.stops).toHaveLength(2);
+    expect(dt.stops).toHaveLength(3);
   });
 });
-
-const makeFixtures = async (
-  residenceModel: MongooseModel,
-  stopModel: MongooseModel,
-  routingModel: MongooseModel,
-): Promise<void> => {
-  const [dt, pth] = await residenceModel.insertMany([
-    { slug: 'dt', name: 'Deux Tours' },
-    { slug: 'pth', name: 'パークタワー晴海' },
-  ]);
-  const [hrm3, hmc] = await stopModel.insertMany([
-    { name: '晴海三丁目' },
-    { name: 'ホテルマリナーズコート前' },
-  ]);
-
-  for (const residence of [dt, pth]) {
-    await routingModel.update(
-      { residence: residence._id, stop: hrm3._id, distance: 2 },
-      {},
-      { upsert: true },
-    );
-  }
-
-  await routingModel.update(
-    { residence: dt._id, stop: hmc._id, distance: 1 },
-    {},
-    { upsert: true },
-  );
-};
--- src/data/db.ts
+++ src/data/db.ts
@@ -1,28 +1,37 @@
-import mongoose from 'mongoose';
+import mongoose, { Connection, Model, Document } from 'mongoose';
 
 import { ResidenceSchema } from './models/Residence';
-import { RoutingSchema } from './models/Routing';
 import { StopSchema } from './models/Stop';
+import { RoutingSchema } from './models/Routing';
 
-const connect = async () => {
-  const connection = await mongoose.createConnection(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
+export type MongooseModel = Model<Document, {}>;
 
-  const Residence = connection.model('Residence', ResidenceSchema);
-  const Stop = connection.model('Stop', StopSchema);
-  const Routing = connection.model('Routing', RoutingSchema);
+export const createModels = (connection: Connection) => ({
+  Residence: connection.model('Residence', ResidenceSchema),
+  Stop: connection.model('Stop', StopSchema),
+  Routing: connection.model('Routing', RoutingSchema),
+});
 
-  return {
-    connection,
-    models: {
-      Residence,
-      Routing,
-      Stop,
-    },
-  };
+export const connect = async () => {
+  let { connection } = mongoose;
+  if (connection.readyState !== connection.states.connected) {
+    connection = await mongoose.createConnection(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  const models = createModels(connection);
+
+  return { connection, models };
+};
+
+export const disconnect = async () => {
+  const { connection } = mongoose;
+  if (connection.readyState === connection.states.connected) {
+    await mongoose.disconnect();
+  }
 };
 
 export default connect;
