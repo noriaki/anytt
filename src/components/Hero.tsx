c32601762896613b8ccee2358652aaa8f763b800
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-28 17:20:06 +0900

    add button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 // material-ui
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const Hero: React.FC = () => (
   <Typography variant="h5" component="h1" color="primary" align="center">
     Next.js with TypeScript
+    <Button variant="contained" color="secondary" fullWidth>
+      Subscribe
+    </Button>
   </Typography>
 );
 
