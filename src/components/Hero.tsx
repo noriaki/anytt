a80f406972411855f7bc3d5a78e3ce7bf3e8ddc8
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-28 19:12:19 +0900

    add CSS Modules

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,19 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
+// styles
+import styles from './Hero.module.css';
+
 const Hero: React.FC = () => (
   <Typography variant="h5" component="h1" color="primary" align="center">
     Next.js with TypeScript
-    <Button variant="contained" color="secondary" fullWidth>
+    <Button
+      variant="contained"
+      color="secondary"
+      classes={{ root: styles.root }}
+      className={styles.big}
+      fullWidth
+    >
       Subscribe
     </Button>
   </Typography>
