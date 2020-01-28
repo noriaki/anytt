98c5bf2683fe2d5110878faf502ab237716a13e7
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-28 17:16:25 +0900

    add material-ui components, styles

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// material-ui
+import Typography from '@material-ui/core/Typography';
+
+const Hero: React.FC = () => (
+  <Typography variant="h5" component="h1" color="primary" align="center">
+    Next.js with TypeScript
+  </Typography>
+);
+
+export default Hero;
