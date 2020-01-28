57b66bb6dbfa5a23f0ef11fa9a47f90458917289
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-28 21:54:35 +0900

    allow styles to be overridden using CSS Modules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import theme from '~/contexts/theme';
@@ -26,9 +26,11 @@ export default class MyApp extends App {
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         </Head>
         <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
+          <StylesProvider injectFirst>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </StylesProvider>
         </ThemeProvider>
       </React.Fragment>
     );
