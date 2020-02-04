c2af1f091034fb342e1c1cd17e87d18dc272e724
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-02-04 14:32:32 +0900

    add fixtures data/types

---
+++ src/data/types.ts
@@ -0,0 +1,40 @@
+export type TimetableHour = {
+  hour: number;
+  minutes: number[];
+};
+
+export type Timetable = {
+  id: string;
+  calendar: number[];
+  data: TimetableHour[] | null;
+};
+
+export type Route = {
+  id: string;
+  headsign: string | null;
+  dest: string;
+  description: string;
+  timetables: Timetable[];
+};
+
+export type Stop = {
+  id: string;
+  name: string;
+  agency: {
+    id: string;
+    name: string;
+  };
+  routes: Route[];
+};
+
+export type StopWithPath = {
+  distance: number;
+  stop: Stop;
+};
+
+export type Residence = {
+  id: string;
+  slug: string;
+  name: string;
+  stops: StopWithPath[];
+};
