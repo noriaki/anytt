03234440092e84ed73611b80c6f6c7a68596dc71
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-02-14 18:26:56 +0900

    remove .env for testing by jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,1 @@
-import { resolve } from 'path';
-import dotenv from 'dotenv';
 import 'jest-enzyme';
-
-dotenv.config({ path: resolve('./.env.test') });
--- src/lib/types/environment.d.ts
+++ src/lib/types/environment.d.ts
@@ -4,6 +4,5 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly MONGODB_URI: string;
-    readonly DB_NAME: string;
   }
 }
