fb67c15ec4b7ede9f5d0bf63abdf10a717db575b
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-02-18 00:53:20 +0900

    remove test that no db access

--- __tests__/data/env.test.ts
+++
@@ -1,6 +0,0 @@
-describe('env', () => {
-  it('should have MONGODB_URI', () => {
-    expect(process.env.MONGODB_URI).toBeDefined();
-    expect(typeof process.env.MONGODB_URI).toBe('string');
-  });
-});
