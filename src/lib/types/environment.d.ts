3f69bf0741f204e3e1ef840c891ff7d88431d971
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-02-17 21:13:52 +0900

    add global types

--- src/lib/types/environment.d.ts
+++ src/lib/types/environment.d.ts
@@ -1,8 +1,18 @@
 /// <reference types="node" />
 
+import { DatabaseInfo } from '~/data/db';
+
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly MONGODB_URI: string;
+  }
+}
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      db: DatabaseInfo;
+    }
   }
 }
