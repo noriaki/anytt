a5f665f00624a03daf1f40eee8fbf2cc3d06939a
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-02-24 10:11:48 +0900

    change dir to `tmp/odpt`

--- src/scripts/odpt/fetch.ts
+++ src/scripts/odpt/fetch.ts
@@ -1,15 +1,20 @@
 import fs from 'fs';
+import path from 'path';
 import fetch from 'node-fetch';
 import yauzl from 'yauzl-promise';
 
 const token = process.env.ODPT_AUTH_TOKEN;
 const uri = `https://api-tokyochallenge.odpt.org/api/v4/files/Toei/data/ToeiBus-GTFS.zip?acl:consumerKey=${token}`;
+const dirPath = './tmp/odpt';
+
+fs.mkdirSync(dirPath, { recursive: true });
 
 fetch(uri).then(async res => {
   const buffer = await res.buffer();
   const zipFile = await yauzl.fromBuffer(buffer, { lazyEntries: true });
   await zipFile.walkEntries(async entry => {
-    const writeStream = fs.createWriteStream(`./tmp/${entry.fileName}`);
+    const filePath = path.resolve(dirPath, entry.fileName);
+    const writeStream = fs.createWriteStream(filePath);
     const readStream = await zipFile.openReadStream(entry);
     readStream.pipe(writeStream);
     console.log(entry.getLastModDate(), entry.fileName);
