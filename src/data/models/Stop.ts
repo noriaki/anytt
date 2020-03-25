eff54b754f4cc5713a4095d6e1d6b95727e76834
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-03-25 17:02:47 +0900

    remove unused models, add bus-stop model with geo

--- src/data/models/Stop.ts
+++ src/data/models/Stop.ts
@@ -1,27 +1,19 @@
-import { createSchema, Type, ExtractDoc, ExtractProps, typedModel } from 'ts-mongoose';
+import { createSchema, Type, typedModel } from 'ts-mongoose';
+
+import GeoSchema from './concerns/Geo';
 
 export const StopSchema = createSchema(
   {
+    __id: Type.string({ required: true, unique: true, index: true }),
     name: Type.string({ required: true, index: true }),
-    // lineId: Type.objectId({ required: true, index: true }),
+    loc: Type.schema({ index: '2dsphere' }).of(GeoSchema),
+    __agencyId: Type.string({ index: true }),
+    __feedVersion: Type.string({ index: true }),
   },
-  { timestamps: true, toJSON: { virtuals: true } },
+  { timestamps: true },
 );
-
-StopSchema.virtual('residences', {
-  ref: 'Routing',
-  localField: '_id',
-  foreignField: 'stop',
-});
 
 export const collectionName = 'Stop';
 export const Stop = typedModel(collectionName, StopSchema);
-
-// class StopClass extends Model {}
-// StopSchema.loadClass(StopClass);
-
-export type StopDocument = ExtractDoc<typeof StopSchema>;
-export type StopProps = ExtractProps<typeof StopSchema>;
-export type StopModel = typeof Stop;
 
 export default Stop;
