2fa274e8bfde30305e6587d541ce05c9b20466e8
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-03-25 16:57:52 +0900

    remove association test, move test helper

---
+++ __tests__/data/fixtures/helper.ts
@@ -0,0 +1,26 @@
+import { DatabaseInfo } from '~/data/db';
+
+// data
+import geosearchFixtures from './geosearch.json';
+
+type Tfixtures = {
+  [target: string]: {
+    model: string;
+    data: any;
+  };
+};
+
+const fixtures: Tfixtures = {
+  geosearch: { model: 'Stop', data: geosearchFixtures },
+};
+
+export const importFixture = (db: DatabaseInfo, target: string) => {
+  const { model, data } = fixtures[target];
+  return db.models[model].insertMany(data);
+};
+
+export const deleteModelsData = (db: DatabaseInfo) => {
+  const models = db.connection.modelNames();
+  const promises = models.map(m => db.connection.models[m].deleteMany({}));
+  return Promise.all(promises);
+};
