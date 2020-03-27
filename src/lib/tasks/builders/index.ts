6c7ec37aff6a239f419d588e462c6b893ee111e0
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-03-27 16:30:46 +0900

    add bulk operations builder of the stops

--- src/lib/tasks/builders/index.ts
+++ src/lib/tasks/builders/index.ts
@@ -3,8 +3,9 @@ import { BulkOperation } from '~/lib/types/mongodb.bulkOps';
 import { GtfsSourceIdentifier } from '../tasks.config';
 
 // builders
-import { setup, teardown } from './feed';
-import buildAgency from './agency';
+import { setup, teardown, extractFeedVersion } from './feed';
+import buildAgency, { extractAgencyId } from './agency';
+import buildStop from './stop';
 
 type BulkOpsWithModel = {
   model: string;
@@ -16,11 +17,12 @@ const buildBulkOperations = async (
   source: GtfsSourceIdentifier,
   dirPath: string,
 ): PromiseReturningBulkOpsWithModels => {
-  // const csv = await readAndParse(dirPath, 'feed_info.txt');
-  // const ops: BulkOpsWithModel[] = [];
+  const agencyId = await extractAgencyId(dirPath);
+  const feedVersion = await extractFeedVersion(dirPath);
   return [
     { model: 'Feed', ops: await setup(source, dirPath) },
     { model: 'Agency', ops: await buildAgency(source, dirPath) },
+    { model: 'Stop', ops: await buildStop(dirPath, agencyId, feedVersion) },
     { model: 'Feed', ops: teardown(source) },
   ];
 };
