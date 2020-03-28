e4dc24398522a6e14038323dc73995ace3754690
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-03-28 23:05:24 +0900

    fix typo

--- __tests__/unit/lib/tasks/builders/agency.test.ts
+++ __tests__/unit/lib/tasks/builders/agency.test.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { extractAgencyId } from '~/lib/tasks/builders/agency';
 
 describe('builders/agency#extractAgencyId', () => {
-  it('should returning `agency_version`:string', async () => {
+  it('should returning `agency_id`:string', async () => {
     const subject = await extractAgencyId(resolve(__dirname, 'fixtures'));
     expect(subject).toBe('8000020130001');
   });
