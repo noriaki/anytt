5acf93b9f8d4adf98d822f30f9cbdb83964928fd
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-03-30 18:33:42 +0900

    research jest testing / .toStrictEqual vs .toMatchObject vs .objectContaining

--- __tests__/unit/sample.test.ts
+++ __tests__/unit/sample.test.ts
@@ -14,4 +14,66 @@ describe('sample', () => {
   it('tsconfig.compilerOptions.jsx === react', () => {
     expect(compilerOptions.jsx).toBe('react');
   });
+
+  /*
+  describe('.toStrictEqual vs .toMatchObject vs .objectContaining', () => {
+    let subject: { position: { x: 0, y: 0 }, other?: string };
+    let expected: { position: { x: 0, y: 0 }, other?: string };
+
+    beforeEach(() => {
+      subject = { position: { x: 0, y: 0 } };
+      expected = { position: { x: 0, y: 0 } };
+    });
+
+    describe('same object', () => {
+      it('.toStrictEqual', () => {
+        expect(subject).toStrictEqual(expected);
+      });
+
+      it('.toMatchObject', () => {
+        expect(subject).toMatchObject(expected);
+      });
+
+      it('.objectContaining', () => {
+        expect(subject).toEqual(expect.objectContaining(expected));
+      });
+    });
+
+    describe('difference object: subject has more props', () => {
+      beforeEach(() => {
+        subject.other = 'other prop';
+      });
+
+      it('.toStrictEqual', () => {
+        expect(subject).toStrictEqual(expected);
+      });
+
+      it('.toMatchObject', () => {
+        expect(subject).toMatchObject(expected);
+      });
+
+      it('.objectContaining', () => {
+        expect(subject).toEqual(expect.objectContaining(expected));
+      });
+    });
+
+    describe('difference object: expected has more props', () => {
+      beforeEach(() => {
+        expected.other = 'other prop';
+      });
+
+      it('.toStrictEqual', () => {
+        expect(subject).toStrictEqual(expected);
+      });
+
+      it('.toMatchObject', () => {
+        expect(subject).toMatchObject(expected);
+      });
+
+      it('.objectContaining', () => {
+        expect(subject).toEqual(expect.objectContaining(expected));
+      });
+    });
+  });
+  */
 });
