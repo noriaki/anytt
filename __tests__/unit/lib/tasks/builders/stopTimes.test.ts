1060fee18de6614640726a9b7333a70db95a3405
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-03-31 20:27:44 +0900

    add tests for trip method

--- __tests__/unit/lib/tasks/builders/stopTimes.test.ts
+++ __tests__/unit/lib/tasks/builders/stopTimes.test.ts
@@ -1,28 +1,47 @@
 import { resolve } from 'path';
-import { combineTimetable } from '~/lib/tasks/builders/stopTimes';
+import { combineTimetable, createMapTripToRouteAndService } from '~/lib/tasks/builders/stopTimes';
 import { parseCsvSync, CsvRowAsObj } from '~/lib/tasks/builders/utils';
 
-describe('builders/stopTimes#combineTimetable', () => {
-  let data: CsvRowAsObj[];
-  beforeEach(() => {
-    data = parseCsvSync(resolve(__dirname, 'fixtures'), 'stop_times.txt');
+// data
+import tripsMap from './fixtures/trips_map.json';
+
+describe('builders/stopTimes', () => {
+  describe('#combineTimetable', () => {
+    let data: CsvRowAsObj[];
+    beforeEach(() => {
+      data = parseCsvSync(resolve(__dirname, 'fixtures'), 'stop_times.txt');
+    });
+
+    it('should returning includes sorted data: number[]', () => {
+      const expected = {
+        stop_id: '1253-01',
+        route_id: '72101-2',
+        service_id: '81-170',
+        data: [24540, 27720, 30660, 37740, 44100, 59340],
+      };
+      const subjects = combineTimetable(data);
+      expect(subjects).toHaveLength(5);
+      const subject = subjects.find(
+        (s) =>
+          s.stop_id === expected.stop_id &&
+          s.route_id === expected.route_id &&
+          s.service_id === expected.service_id,
+      );
+      expect(subject).toMatchObject(expected);
+    });
   });
 
-  it('should returning ', () => {
-    const expected = {
-      stop_id: '1253-01',
-      route_id: '72101-2',
-      service_id: '81-170',
-      data: [24540, 27720, 30660, 37740, 44100, 59340],
-    };
-    const subjects = combineTimetable(data);
-    expect(subjects).toHaveLength(5);
-    const subject = subjects.find(
-      (s) =>
-        s.stop_id === expected.stop_id &&
-        s.route_id === expected.route_id &&
-        s.service_id === expected.service_id,
-    );
-    expect(subject).toMatchObject(expected);
+  describe('#createMapTripToRouteAndService', () => {
+    let data: CsvRowAsObj[];
+    beforeEach(() => {
+      data = parseCsvSync(resolve(__dirname, 'fixtures'), 'stop_times_raw.txt');
+    });
+
+    it('should returning mapper functions', () => {
+      const expected = parseCsvSync(resolve(__dirname, 'fixtures'), 'stop_times.txt');
+      const mapper = createMapTripToRouteAndService(tripsMap);
+      const subjects = data.map(mapper);
+      expect(subjects).toStrictEqual(expected);
+    });
   });
 });
