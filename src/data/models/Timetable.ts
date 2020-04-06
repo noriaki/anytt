067498fd073fb5deb42092c7b66a9ea0491d9153
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-04-06 11:09:12 +0900

    fix not found column error

--- src/data/models/Timetable.ts
+++ src/data/models/Timetable.ts
@@ -2,9 +2,9 @@ import { createSchema, Type, typedModel } from 'ts-mongoose';
 
 export const TimetableSchema = createSchema(
   {
-    __id: Type.string({ required: true, unique: true, index: true }),
     schedule: Type.array().of(Type.number()),
     data: Type.array().of(Type.number()),
+    __stopId: Type.string({ index: true }),
     __routeId: Type.string({ index: true }),
     __serviceId: Type.string({ index: true }),
     __agencyId: Type.string({ index: true }),
